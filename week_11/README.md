9682e14334574897e73e7b2347949c588827fc62
Joel Whiteman <joel.whiteman@ccac.edu>
2020-04-22 17:48:05 -0400

    Adding a simple script for file tree traversal

---
+++ week_11/README.md
@@ -0,0 +1,9 @@
+# Using tree_traversal.py
+
+tree_traversal.py is a simple script that traverses a file tree to count the number of files in it, the number of .jpgs in it, and the average 
+filesize (in KB).
+
+It takes a directory filepath as an input argument. A sample directory with several jpeg files is included in OneDrive_1_4-15-2020.zip. To run
+the script with this sample directory, simply type:
+
+`python tree_traversal.py -d HorrorMovies_SaraD`
---
+++ week_11/tree_traversal.py
@@ -0,0 +1,81 @@
+import os
+import re
+import argparse
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--directory', required=True, help="""Path to the directory 
+        you'd like to traverse.""")
+    args = parser.parse_args()
+    dir_path = args.directory
+    return dir_path
+
+def extension_regexp():
+# regular expression that matches a . followed by alpha characters, $ starts from end of string
+# important to remember that this regexp will match any number of cases in the string
+    regexp = re.compile('(\.\w+)$')
+    return regexp
+
+def traverse(start_loc, regexp):
+    total_file = 0
+    total_jpeg = 0
+    total_kb = 0
+    for root_dir, directories, files in os.walk(start_loc):
+        dir_file_count, dir_jpeg_count, dir_total_kb = examine_file_tree(root_dir, files, regexp)
+        total_file += dir_file_count
+        total_jpeg += dir_jpeg_count
+        total_kb += dir_total_kb
+    print_total_information(total_file, total_jpeg, total_kb)
+
+def examine_file_tree(root_dir, filelist, regexp):
+    dir_file_count = 0
+    dir_jpeg_count = 0
+    dir_total_kb = 0
+    for file in filelist:
+        # os.sep translates to \ or / depending on the os and filesystem
+        filepath = root_dir + str(os.sep) + file
+        if os.path.isfile(filepath):
+            extension, kb_filesize = get_file_information(filepath, regexp)
+            dir_file_count += 1
+        if extension == '.jpg':
+            dir_jpeg_count += 1
+        dir_total_kb += kb_filesize
+    return dir_file_count, dir_jpeg_count, dir_total_kb
+            
+
+def get_file_information(filepath, regexp):
+    kb_filesize = (os.path.getsize(filepath) / 10**3)
+    extension = False
+    # creates a boolean match object by searching the regexp against the filepath string
+    match = re.search(regexp, filepath)
+    if match:
+        # only matches the first . followed by alpha characters (from end of string), i.e. the extension
+        extension = match.group(0)
+    print_file_information(filepath, kb_filesize, extension)
+    return extension, kb_filesize
+
+def print_file_information(filepath, kb_filesize, extension):
+    print('filepath:', filepath)
+    print('file size: ', end='')
+    print(kb_filesize, 'KB')
+    if extension:
+        print('extension:', extension)
+    if not extension:
+        print('extension: file has no extension' )
+
+def print_total_information(total_file, total_jpeg, total_kb):
+    avg_file_size = (total_kb / total_file)
+    print('\n*****************************')
+    print('Number of file objects:', total_file)
+    print('Number of .jpegs:', total_jpeg)
+    print('Average file size:', avg_file_size, 'KB')
+
+def main():
+# starts walking from the directory where script is run
+    dir_path = parse_arguments()
+    regexp = extension_regexp()
+    traverse(dir_path, regexp)
+
+if __name__ == "__main__":
+    main()
+
